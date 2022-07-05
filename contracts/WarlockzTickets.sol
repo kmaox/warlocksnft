a7dcb489cc4e26c4387575d6a7a914468b2eb1fe
Kevin Mao <90167810+kmaox@users.noreply.github.com>
2022-07-05 17:47:39 +1000

    Update WarlockzTickets.sol

--- contracts/WarlockzTickets.sol
+++ contracts/WarlockzTickets.sol
@@ -1,44 +1,44 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity >=0.6.12 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "./WarlockzVRF.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-interface iRedeem {
-    function transferFrom(
-        address from,
-        address to,
-        uint256 tokenId
-    ) external;
-}
+contract WarlockzTickets is ERC721, Ownable, ReentrancyGuard {
 
-contract tickets is WarlockzVRF {
-    uint64 private subscriptionId = 6947;
-    uint256 requiredTickets = 1;
-    uint64 bTicket;
-    uint64 mTicket;
-    uint64 sTicket;
-    iRedeem public tRedeem;
+    event BronzeTicketMinted (uint256 tokenID);
+    event SliverTicketMinted (uint256 tokenID);
+    event GoldTicketMinted (uint256 tokenID);
 
-    constructor() WarlockzVRF(subscriptionId) {}
+    uint256 tokenIDToMint = 1;
+    mapping(uint256 => uint256) ticketRarity;
 
-    function bRedeem() public enoughTickets {
-        addressToTicket[msg.sender] -= bTicket;
-        //tRedeem.transferFrom("contract that holds the nft that the user is redeeming", msg.sender, "token id of nft");
-    }
+    constructor() ERC721("Warlockz Tickets", "WTICK"){}
 
-    function mRedeem() public enoughTickets {
-        addressToTicket[msg.sender] -= mTicket;
-        //tRedeem.transferFrom("contract that holds the nft that the user is redeeming", msg.sender, "token id of nft");
-    }
-
-    function sRedeem() public enoughTickets {
-        addressToTicket[msg.sender] -= sTicket;
-        //tRedeem.transferFrom("contract that holds the nft that the user is redeeming", msg.sender, "token id of nft");
-    }
-
-    modifier enoughTickets() {
-        require(addressToTicket[msg.sender] >= requiredTickets);
-        _;
+    function mintTicket(uint256 _amount) external {
+        if (_amount == 1) {
+            ticketRarity[tokenIDToMint] = _amount; 
+            _mint(msg.sender, tokenIDToMint);
+            tokenIDToMint++;
+            emit BronzeTicketMinted(tokenIDToMint);
+        }
+        if (_amount == 2) {
+            ticketRarity[tokenIDToMint] = _amount; 
+            _mint(msg.sender, tokenIDToMint);
+            tokenIDToMint++;
+            emit SliverTicketMinted(tokenIDToMint);
+        }
+        if (_amount == 3) {
+            ticketRarity[tokenIDToMint] = _amount; 
+            _mint(msg.sender, tokenIDToMint);
+            tokenIDToMint++;
+            emit GoldTicketMinted(tokenIDToMint);
+        }
     }
 }
+
+
+
